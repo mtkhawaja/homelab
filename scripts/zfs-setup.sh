a205ad7d5077bdb044318dcb177e66fd7056a45d
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2025-07-05 02:49:10 -0400

    fix: Added zfs snippet.

--- scripts/zfs-setup.sh
+++ scripts/zfs-setup.sh
@@ -11,3 +11,6 @@ sudo fdisk -l
 sudo zpool create -f <pool-name-1> "space delimited devices go here"
 sudo zpool create -f <pool-name-2> "space delimited devices go here"
 sudo zpool status
+
+# zpool disappears after reboot
+sudo zpool import
